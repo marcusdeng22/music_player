07408a8856c1250db8649a8928874e852ee6564e
marcus <marcusd.9234@gmail.com>
2020-01-21 22:45:39 -0600

    some recommendations pulled, bad formatting

--- src/www/static/js/play.js
+++ src/www/static/js/play.js
@@ -91,6 +91,19 @@ app.controller('playCtrl', ["$scope", "$timeout", "$location", "$http", "uiSorta
 		$scope.nowPlayingIndex = data.index;
 		// userSet = false;
 		console.log(data);
+		//set the reccs here
+		console.log("TRACK CHANGED");
+		console.log($scope.nowPlaying);
+		//from: http://www.whateverorigin.org/
+		$.getJSON('http://www.whateverorigin.org/get?url=' + encodeURIComponent($scope.nowPlaying["url"]) + '&callback=?', function(data){
+			// alert(data.contents);
+			if (data && data != null && typeof data == "object" && data.contents && data.contents != null && typeof data.contents == "string") {
+				//from: https://stackoverflow.com/questions/6659351/removing-all-script-tags-from-html-with-js-regular-expression
+				var reccHtml = data.contents.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, "");
+				console.log(reccHtml);
+				$("#recommendedHolder").html($("li.video-list-item.related-list-item.show-video-time.related-list-item-compact-video", reccHtml));
+			}
+		});
 	});
 
 
